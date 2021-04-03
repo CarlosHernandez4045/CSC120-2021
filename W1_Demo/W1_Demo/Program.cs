176f0613957b5334eee08bc23f1e0a8933e6311f
Carlos Hernandez <carloshernandez4045@gmail.com>
2021-04-02 17:09:17 -0700

    first commit

---
+++ W1_Demo/W1_Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace W1_Demo
+{
+    class Program
+    {
+        static void Main(string[] args) //this is an entry point, the main method gets executed first.
+        {
+            var qty = 112.23;
+            var unitPrice = 4.90;
+
+            var amount = Calculate((int)qty, unitPrice);
+            Console.WriteLine("The amount owed = " + amount);
+        }
+
+        private static double Calculate(int qty, double unitPrice)
+        {
+            var finalCalc = qty * unitPrice;
+
+            return finalCalc;
+
+        }
+    }
+}
