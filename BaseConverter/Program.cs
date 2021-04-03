a505dcd4cdb0286df2587158af324c1ec0461b0a
Carlos Hernandez <carloshernandez4045@gmail.com>
2021-04-02 17:09:45 -0700

    2nd commit

---
+++ BaseConverter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BaseConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Any Number:");
+            int number = Int32.Parse(Console.ReadLine());
+            string convertFinal = Convert.ToString(number, 8);
+            Console.WriteLine(convertFinal);
+        }
+    }
+}
