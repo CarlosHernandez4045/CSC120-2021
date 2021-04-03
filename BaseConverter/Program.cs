0974aaca5f6e64ab069d3221c560baf4c06d0639
Carlos Hernandez <carloshernandez4045@gmail.com>
2021-04-02 17:33:54 -0700

    Base Number System Assignment / 4th commit

--- BaseConverter/Program.cs
+++ BaseConverter/Program.cs
@@ -6,10 +6,28 @@ namespace BaseConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Any Number:");
-            int number = Int32.Parse(Console.ReadLine());
-            string convertFinal = Convert.ToString(number, 8);
-            Console.WriteLine(convertFinal);
+            Console.WriteLine("Enter any number to convert to base 8, enter 0 to exit:");
+            convertToBaseEight();
+        }
+        static void convertToBaseEight()
+        {
+            while (true)
+            {
+                int number = Int32.Parse(Console.ReadLine());
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("Invalid Input, Try Again");
+                }
+                else
+                {
+                    string convertFinal = Convert.ToString(number, 8);
+                    Console.WriteLine("The base 8 version is " + convertFinal);
+                }
+            }
         }
     }
 }
