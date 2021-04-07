eb3b6b3b2d71208461431cfccd517d189b8d4e66
Carlos Hernandez <carloshernandez4045@gmail.com>
2021-04-06 23:38:51 -0700

    11th commit / removing side projects

---
+++ W2DigitalGates/OrGate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2DigitalGates
+{
+    class OrGate
+    {
+        public static bool Input(bool x, bool y)
+        {
+            if (x.Equals(true) || y.Equals(true))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ W2DigitalGates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace W2DigitalGates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var truthTable = new List<GateInputs>();
+            truthTable.Add(new GateInputs { X = false, Y = false });
+            truthTable.Add(new GateInputs { X = true, Y = false });
+            truthTable.Add(new GateInputs { X = false, Y = true });
+            truthTable.Add(new GateInputs { X = true, Y = true });
+            Console.WriteLine("X | Y | Z");
+            foreach (var row in truthTable)
+            {
+                var result = OrGate.Input(row.X, row.Y);
+                Console.WriteLine("____________");
+                string xas = row.X ? "1" : "0";
+                string yas = row.Y ? "1" : "0";
+                var resultAs = result ? "1" : "0";
+                Console.WriteLine($"{xas} | {yas} | {resultAs}");
+            }
+        }
+        //static void Test1()
+        //{
+        //    var x = true;
+        //    var y = false;
+        //    var result = OrGate.Input(x, y);
+        //    Console.WriteLine($"The result of AndGate for inputs of" +
+        //        $" X = {x} and Y = {y} is equal to {result}");
+        //}
+    }
+}
