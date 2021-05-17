d0420490137e442ee84baabd5974977164af02d1
Carlos Hernandez <carloshernandez4045@gmail.com>
2021-05-17 11:09:54 -0700

    Functioning 10var mux gen/eval / beginning to SimpleCircuits

---
+++ SimpleCircuits/Graph.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCircuits
+{
+    class Graph
+    {
+        private static void printSpaces()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+        private static void printLine()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write("-");
+            }
+        }
+        private static void topLine(bool value)
+        {
+
+        }
+        private static void topLine()
+        {
+
+        }
+
+        private static void BackBone(bool result)
+        {
+            Console.WriteLine("|");
+            Console.WriteLine("|");
+            if (result)
+            {
+                //start calling topLine method here
+            }
+
+            Console.WriteLine("|");
+            Console.WriteLine("|");
+            Console.WriteLine("|");
+            Console.WriteLine("|");
+        }
+        public static void PrintGraph(string input, bool result )
+        {
+            //coming back after, pass val to it, then it calls other methods to either print space or line? 
+        }
+    }
+}
---
+++ SimpleCircuits/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleCircuits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input.Generate(10); //will generate whatever amount of binary inputs. in this case 10
+
+            var logicList = Input.ConvertToLogicList();
+
+            foreach(var item in logicList)
+            {
+                var gateEval = new LogicGate()
+                {
+                    Input1 = item.Input1,
+                    Input2 = item.Input2,
+                    Input3 = item.Input3,
+                    Input4 = item.Input4,
+                    Input5 = item.Input5,
+                    Input6 = item.Input6,
+                    Input7 = item.Input7,
+                    Input8 = item.Input8,
+                    Input9 = item.Input9,
+                    Input10 = item.Input10,
+
+                };
+                bool result = gateEval.TenVar();
+                string inputs = $"{Function.ConvertToBinary(item.Input1)}{Function.ConvertToBinary(item.Input2)}{Function.ConvertToBinary(item.Input3)}{Function.ConvertToBinary(item.Input4)}" +
+                    $"{Function.ConvertToBinary(item.Input5)}{Function.ConvertToBinary(item.Input6)}{Function.ConvertToBinary(item.Input7)}" +
+                    $"{Function.ConvertToBinary(item.Input8)}{Function.ConvertToBinary(item.Input9)}{Function.ConvertToBinary(item.Input10)}";
+                Console.WriteLine($"Inputs: {inputs}");
+                if (result)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Result: {Function.ConvertToBinary(result)}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Result: {Function.ConvertToBinary(result)}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            
+            Console.ReadKey(true);
+        }
+    }
+}
